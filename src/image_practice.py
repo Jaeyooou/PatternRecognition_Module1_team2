efd86dff57b31496fb5bf0fd7ef21326ada1dbb5
jaeyooouinthebuildin <jaehyen2@naver.com>
2021-03-31 02:17:36 +0900

    컨투어 기준으로 사각형을 만들고 자른뒤 확대하는 코드 작성

--- src/image_practice.py
+++ src/image_practice.py
@@ -8,26 +8,32 @@ import numpy as np
 # 4단계 : Hyteresis Thresholding : 3단계를 거친 이미지가 실제 엣지인지 판단하는 부분
 def FindContour(path):
      src = cv2.imread(path , cv2.IMREAD_COLOR)
+     contour_src = src.copy()
      gray = cv2.cvtColor(src , cv2.COLOR_BGR2GRAY)
      ret,binary = cv2.threshold(gray , 127 , 255 , cv2.THRESH_BINARY)
      binary = cv2.bitwise_not(binary)
-     cv2.imshow("srcr",src)
      contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
      print(len(contours))
      for cnt in contours:
           x , y, w , h = cv2.boundingRect(cnt)
-          cv2.rectangle(src ,(x,y) , (x+w,y+h) ,(0,255,0) ,2)
-          print(x , y,  w, h)
-
+          cv2.rectangle(contour_src ,(x,y) , (x+w,y+h) ,(0,255,0) ,1) # 초록색으로 컨투어 검출해서 직사각형 찍기
+          crop_img = src[y:y+h , x:x+w] # 직사각형 기준으로 자른 이미지
+          crop_upper_img = cv2.resize(crop_img , (400,400)) # 자른이미지 400*400 으로 확대
+          print(x , y,  w, h) # 컨투어 좌표 x , y, 가로 , 세로
           break # 만약 숫자 안에 도형이 하나 더 있으면 안찍히게 하기 위해서
 
-     cv2.imshow("src",src)
+######################################### 출력부분
+     cv2.imshow("src",src) # 원본
+     cv2.imshow("contour_image" , contour_src) # 컨투어한 사각형 이미
+     cv2.imshow("crop",crop_img)
+     cv2.imshow("crop & upper" , crop_upper_img)
+
      cv2.waitKey(0)
      cv2.destroyWindow()
 
 
-path= 'resource/Goolim/1.png'
+path= 'resource/Godic/8.png'
 FindContour(path)
 # dst = src.copy()
 # gray = cv2.cvtColor(src , cv2.COLOR_BGR2GRAY)
